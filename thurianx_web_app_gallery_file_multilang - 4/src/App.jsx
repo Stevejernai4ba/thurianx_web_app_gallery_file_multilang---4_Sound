6c9e71cbdfafd709f2d64d1ece26eed1d82d6606
ThurianX <138269799+Stevejernai4ba@users.noreply.github.com>
2025-05-25 00:01:51 +0700

    Update App.jsx

--- thurianx_web_app_gallery_file_multilang - 4/src/App.jsx
+++ thurianx_web_app_gallery_file_multilang - 4/src/App.jsx
@@ -128,7 +128,6 @@ function WelcomeScreen({ onStart, lang, setLang }) {
 
 
 
-import React, { useState, useRef } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 
 function getResultStyle(index) {
